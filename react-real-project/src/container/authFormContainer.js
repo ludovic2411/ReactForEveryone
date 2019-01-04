bde01dbf50d930c53ac03693623215d4eb1fcf7f
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-01-04 13:30:43 +0100

    create-state and bind functions

--- react-real-project/src/container/authFormContainer.js
+++ react-real-project/src/container/authFormContainer.js
@@ -4,13 +4,29 @@ import FormButton from '../presentationnal/button';
 
 class authFormContainer extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state ={login:"empty",
+    name: "unknown"} ;
+    this.handleName=this.handleName.bind(this);
+    this.handleLogin=this.handleLogin.bind(this);
+  }
+
+  handleLogin(){
+    this.setState({login:"updated"});
+  }
+
+  handleName(){
+    this.setState({name:"anonymous"});
+  }
+
   render() {
     return (
       <div>
         <form>
           <Input  text="Your name" inputType="text" placeholder="Your Name"/>
           <Input text="Your password" inputType="password" placeholder=""/>
-          <FormButton buttonText="Log in" function={()=>{alert('click')}}/>
+          <FormButton buttonText="Log in" function={()=>{alert(this.state.name)}}/>
         </form>
       </div>
     );
